cf793af396c1049663e4f6233df4e523e701ab4c
DomenicoLerose85 <domenicolerose85@libero.it>
2022-06-23 16:01:41 +0200

    first commit

---
+++ resources/js/components/AppMain.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ titolo }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppMain",
+  data() {
+    return {
+      titolo: "Work in progress",
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
